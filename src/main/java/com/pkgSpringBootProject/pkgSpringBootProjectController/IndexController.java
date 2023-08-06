18d28dc7b26238176f705cd6269c54a80bd82a4e
Alison97 <alison16.d@gmail.com>
2023-08-06 02:08:30 -0500

    practice java

--- src/main/java/com/pkgSpringBootProject/pkgSpringBootProjectController/IndexController.java
+++ src/main/java/com/pkgSpringBootProject/pkgSpringBootProjectController/IndexController.java
@@ -2,13 +2,13 @@ package com.pkgSpringBootProject.pkgSpringBootProjectController;
 
 
 import com.pkgSpringBootProject.Model.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.sql.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,32 +16,54 @@ import java.util.List;
 @RequestMapping("/app")
 public class IndexController {
 
+    @Value("${texto.indexcontroller.index.ti tulo}")
+    private  String textoIndex;
+
+    @Value("${texto.indexcontroller.poblarUsuario.titulo}")
+    private  String textoPoblarUsuario;
+
+    @Value("${texto.indexcontroller.listar.titulo}")
+    private  String textoListar;
+
     @GetMapping({"/index", "/", "", "/home"})
     //method
     public String index(Model model) {
-        model.addAttribute("Titulo", "Hellouuu Spring practician");
+        model.addAttribute("Titulo", textoIndex);
 
         return ("index");
     }
 
     @RequestMapping("/perfil") //si por defecto no se indica el metodo, por defecto es get
     public String perfil(Model model) {
+
+        model.addAttribute("userProfile", "User Profile "); //nombramiento navegador
+
+        return "perfil";
+    }
+
+    @ModelAttribute("usuario")
+    public User poblarUsuario (Model model) {
         User usuario = new User();
         usuario.setNombre("Alison");
         usuario.setApellido("Duque");
         usuario.setEmail("nopi@hotmail.com");
-        model.addAttribute("usuario", usuario);
-        model.addAttribute("userProfile", "User Profile ");
-        model.addAttribute("Titulo", "Datos del usuario: ".concat(usuario.getNombre()).concat(usuario.getApellido()));
-        return "perfil";
+
+        model.addAttribute("usuario", usuario); //expone mi objeto User creado
+        model.addAttribute("Titulo", textoPoblarUsuario.concat(usuario.getNombre()).concat(usuario.getApellido()));
+        return usuario;
     }
 
     @RequestMapping("/listar") //si por defecto no se indica el metodo, por defecto es get
     public String listar(Model model) {
+        model.addAttribute("Titulo", textoListar);
+        return "listar";
+    }
+
+    @ModelAttribute("usuarios") //usuarios es el nombre que recibe en el template listar para mostrar en pantalla al llamar el return listar anterior se ejecuta todo
+    public List<User> poblarUsuarios (){
         List<User> usuarios = Arrays.asList(new User("Alison","Duque", "nopi@hotmail.com"),
                 new User("Sebastian","Narvaez", "nopi2@hotmail.com"));
-        model.addAttribute("Titulo", "Listado de usuarios");
-        model.addAttribute("usuarios",usuarios);
-        return "listar";
+
+        return usuarios;
     }
 }
